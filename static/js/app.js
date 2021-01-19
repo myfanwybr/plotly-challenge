a1673446a9ccc6c7c5c3dfb822d27bce1770a4f0
myfanwybr <m.brownrobinson@hotmail.com>
2021-01-18 23:54:56 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,23 @@
-var data =d3.json("data/samples.json");
+var response =d3.json("data/samples.json"). then(sample=>console.log(sample));
 
-console.log(data)
+
+
+function unpack(rows, index) {
+    return rows.map(function(row) {
+      return row[index];
+    });
+  }
+ 
+  
+// Fetch data & build bar graph
+
+function buildBar(){
+    d3.json("data/samples.json").then(function(data) {
+
+    //grab values from data json object
+        var sample_values=unpack(data.samples.sample_values, );
+        //var otu_ids
+       // var otu_labels
+})}
+
+buildBar(response)
